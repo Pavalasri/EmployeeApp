430403eaa78c3b0a7262fa5241f2bf3e7d7435b8
Pavalasri <pavalasrikannan0005@gmail.com>
2026-05-19 14:36:08 +0530

    Added leave request form UI

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const form = document.getElementById("leaveForm");
+
 const tableBody = document.getElementById("tableBody");
 
 let leaves = JSON.parse(localStorage.getItem("leaves")) || [];
 
 displayLeaves();
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function(e){
+
     e.preventDefault();
 
     const leave = {
+
         empId: document.getElementById("empId").value,
+
         name: document.getElementById("name").value,
+
         type: document.getElementById("leaveType").value,
+
         from: document.getElementById("fromDate").value,
+
         to: document.getElementById("toDate").value
     };
 
@@ -23,31 +30,39 @@ form.addEventListener("submit", function(e) {
     displayLeaves();
 
     form.reset();
+
 });
 
-function displayLeaves() {
+function displayLeaves(){
+
     tableBody.innerHTML = "";
 
-    leaves.forEach((leave, index) => {
+    leaves.forEach((leave, index)=>{
+
         tableBody.innerHTML += `
-            <tr>
-                <td>${leave.empId}</td>
-                <td>${leave.name}</td>
-                <td>${leave.type}</td>
-                <td>${leave.from}</td>
-                <td>${leave.to}</td>
-                <td>
-                    <button onclick="deleteLeave(${index})">
-                        Delete
-                    </button>
-                </td>
-            </tr>
+
+        <tr>
+
+            <td>${leave.empId}</td>
+            <td>${leave.name}</td>
+            <td>${leave.type}</td>
+            <td>${leave.from}</td>
+            <td>${leave.to}</td>
+
+            <td>
+                <button onclick="deleteLeave(${index})">
+                    Delete
+                </button>
+            </td>
+
+        </tr>
         `;
     });
 }
 
-function deleteLeave(index) {
-    leaves.splice(index, 1);
+function deleteLeave(index){
+
+    leaves.splice(index,1);
 
     localStorage.setItem("leaves", JSON.stringify(leaves));
 
